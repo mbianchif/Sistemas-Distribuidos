ca2852257d28ad1a221c1ed1dcd5e77b418c6449
LGhosn <lghosn@fi.uba.ar>
2025-04-12 23:17:47 -0300

    divido download/unzip

--- download-dataset.sh
+++ download-dataset.sh
@@ -2,7 +2,6 @@
 
 # Install dependencies
 sudo apt install curl
-sudo apt install unzip
 
 DIR=.data
 ZIP=dataset.zip
@@ -10,16 +9,3 @@ ZIP=dataset.zip
 # Download and unzip dataset
 mkdir $DIR
 curl -L -o $DIR/$ZIP https://www.kaggle.com/api/v1/datasets/download/rounakbanik/the-movies-dataset
-
-# Extract files
-unzip -j $DIR/$ZIP "movies_metadata.csv" -d $DIR
-unzip -j $DIR/$ZIP "ratings.csv" -d $DIR
-unzip -j $DIR/$ZIP "credits.csv" -d $DIR
-
-# Make small versions of the dataset if n was provided
-if [ $# -eq 1 ]; then
-    N=$(($1 + 1))
-    head -n $N "$DIR/movies_metadata.csv" > temp && mv temp "$DIR/movies_metadata.csv"
-    head -n $N "$DIR/ratings.csv" > temp && mv temp "$DIR/ratings.csv"
-    head -n $N "$DIR/credits.csv" > temp && mv temp "$DIR/credits.csv"
-fi
