41323e598c0947809c48ff2dc7fda1743b465d26
mbianchif <89595589+mbianchif@users.noreply.github.com>
2025-04-19 20:49:54 -0300

    refactor del compose

--- gateway/rabbit/broker.py
+++ gateway/rabbit/broker.py
@@ -1,32 +1,30 @@
-import os
 import pika
 import logging
 
 
 class Broker:
     def __init__(self):
-        self._output_exchange_name = os.getenv("OUTPUT_EXCHANGE_NAME", "")
-        self._output_exchange_type = os.getenv("OUTPUT_EXCHANGE_TYPE", "")
-        self._output_queue_names = os.getenv("OUTPUT_QUEUE_NAMES", "").split(",")
-        self._output_queue_keys = os.getenv("OUTPUT_QUEUE_KEYS", "").split(",")
         logging.getLogger("pika").setLevel(logging.WARNING)
+        self._exchange_name = "gateway"
+        exchange_type = "direct"
+        qs = ["movies", "credits", "ratings"] # Same as routing keys
 
         try:
-            conn = pika.BlockingConnection(pika.ConnectionParameters(host="rabbitmq"))
-            self.channel = conn.channel()
+            self._conn = pika.BlockingConnection(pika.ConnectionParameters(host="rabbitmq"))
+            self._chan = self._conn.channel()
 
-            self.channel.exchange_declare(
-                exchange=self._output_exchange_name,
-                exchange_type=self._output_exchange_type,
+            self._chan.exchange_declare(
+                exchange=self._exchange_name,
+                exchange_type=exchange_type,
                 durable=True,
             )
 
-            for i, queue in enumerate(self._output_queue_names):
-                self.channel.queue_declare(queue=queue)
-                self.channel.queue_bind(
-                    exchange=self._output_exchange_name,
+            for queue in qs:
+                self._chan.queue_declare(queue=queue)
+                self._chan.queue_bind(
+                    exchange="gateway",
                     queue=queue,
-                    routing_key=self._output_queue_keys[i],
+                    routing_key=queue,
                 )
 
         except Exception as e:
@@ -35,9 +33,13 @@ class Broker:
 
     def publish(self, routing_key: str, body: str):
         try:
-            self.channel.basic_publish(
-                exchange=self._output_exchange_name, routing_key=routing_key, body=body
-            )
+            self._chan.basic_publish(exchange=self._exchange_name, routing_key=routing_key, body=body)
         except Exception as e:
             logging.critical(f"Failed to publish message {e}")
             raise
+
+    def close(self):
+        if self._chan and self._chan.is_open:
+            self._chan.close()
+        if self._conn and self._conn.is_open:
+            self._conn.close()
